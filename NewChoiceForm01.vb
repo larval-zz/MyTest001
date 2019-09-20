f97504d0a0b7996e93bc5ca0c02230ee34d63f17
larval-zz <larval@163.com>
2019-09-20 17:42:01 +0800

    Add files via upload

--- NewChoiceForm01.vb
+++ NewChoiceForm01.vb
@@ -1,69 +1,92 @@
 ﻿Public Class NewChoiceForm01
-
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Me.Close()
     End Sub
 
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
-
+        Dim ck1 As Boolean
+        Dim ck2 As Boolean
+        Dim ck3 As Boolean
+        Dim ck4 As Boolean
+        ck1 = CheckBox1.Checked
+        ck2 = CheckBox2.Checked
+        ck3 = CheckBox3.Checked
+        ck4 = CheckBox4.Checked
+        If ck1 = True Then
+            CheckBox2.Enabled = False
+            CheckBox3.Enabled = False
+            CheckBox4.Enabled = False
+        Else
+            CheckBox1.Enabled = True
+            CheckBox2.Enabled = True
+            CheckBox3.Enabled = True
+            CheckBox4.Enabled = True
+        End If
     End Sub
 
     Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
-
+        Dim ck1 As Boolean
+        Dim ck2 As Boolean
+        Dim ck3 As Boolean
+        Dim ck4 As Boolean
+        ck1 = CheckBox1.Checked
+        ck2 = CheckBox2.Checked
+        ck3 = CheckBox3.Checked
+        ck4 = CheckBox4.Checked
+        If ck2 = True Then
+            CheckBox1.Enabled = False
+            CheckBox3.Enabled = False
+            CheckBox4.Enabled = False
+        Else
+            CheckBox1.Enabled = True
+            CheckBox2.Enabled = True
+            CheckBox3.Enabled = True
+            CheckBox4.Enabled = True
+        End If
     End Sub
 
     Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
-
+        Dim ck1 As Boolean
+        Dim ck2 As Boolean
+        Dim ck3 As Boolean
+        Dim ck4 As Boolean
+        ck1 = CheckBox1.Checked
+        ck2 = CheckBox2.Checked
+        ck3 = CheckBox3.Checked
+        ck4 = CheckBox4.Checked
+        If ck3 = True Then
+            CheckBox1.Enabled = False
+            CheckBox2.Enabled = False
+            CheckBox4.Enabled = False
+        Else
+            CheckBox1.Enabled = True
+            CheckBox2.Enabled = True
+            CheckBox3.Enabled = True
+            CheckBox4.Enabled = True
+        End If
     End Sub
 
     Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
-
-    End Sub
-
-    Private Sub CheckBox8_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox8.CheckedChanged
-
-    End Sub
-
-    Private Sub CheckBox7_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox7.CheckedChanged
-
-    End Sub
-
-    Private Sub CheckBox6_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox6.CheckedChanged
-
-    End Sub
-
-    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox5.CheckedChanged
-
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        CheckBox1.Checked = False
-        CheckBox2.Checked = False
-        CheckBox3.Checked = False
-        CheckBox4.Checked = False
-        CheckBox5.Checked = False
-        CheckBox6.Checked = False
-        CheckBox7.Checked = False
-        CheckBox8.Checked = False
-        TextBox1.Text = ""
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If TextBox1.Text = "" Then
-            Dim unused = MessageBox.Show("请输入体检方案的名称！")
-        End If
-    End Sub
-
-    Private Sub NewChoiceForm01_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim ck1 As Boolean
-        Dim cke2 As Boolean
+        Dim ck2 As Boolean
+        Dim ck3 As Boolean
+        Dim ck4 As Boolean
         ck1 = CheckBox1.Checked
-        If ck1 = True Then
-            TextBox1.Text = "ddddddd"
+        ck2 = CheckBox2.Checked
+        ck3 = CheckBox3.Checked
+        ck4 = CheckBox4.Checked
+        If ck4 = True Then
+            CheckBox1.Enabled = False
+            CheckBox2.Enabled = False
+            CheckBox3.Enabled = False
+        Else
+            CheckBox1.Enabled = True
+            CheckBox2.Enabled = True
+            CheckBox3.Enabled = True
+            CheckBox4.Enabled = True
         End If
-
-
-
     End Sub
+
+
 End Class
