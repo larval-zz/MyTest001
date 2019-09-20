d9ff458f7dd6a8f0a5214072a642a8cfa2d94835
恩 姚 <larval@163.com>
2019-09-20 15:51:23 +0800

    Revert "菜单小改01"
    This reverts commit ab782c5d0cfa047633b0fbee9e93432a3b6c3763.

--- NewChoiceForm01.vb
+++ NewChoiceForm01.vb
@@ -1,147 +1,40 @@
 ﻿Public Class NewChoiceForm01
+
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Me.Close()
     End Sub
 
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
-        Dim ck1 As Boolean
-        ck1 = CheckBox1.Checked
-        If ck1 = True Then
-            CheckBox2.Enabled = False
-            CheckBox3.Enabled = False
-            CheckBox4.Enabled = False
-        Else
-            CheckBox1.Enabled = True
-            CheckBox2.Enabled = True
-            CheckBox3.Enabled = True
-            CheckBox4.Enabled = True
-        End If
+
     End Sub
 
     Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
-        Dim ck2 As Boolean
-        ck2 = CheckBox2.Checked
-        If ck2 = True Then
-            CheckBox1.Enabled = False
-            CheckBox3.Enabled = False
-            CheckBox4.Enabled = False
-        Else
-            CheckBox1.Enabled = True
-            CheckBox2.Enabled = True
-            CheckBox3.Enabled = True
-            CheckBox4.Enabled = True
-        End If
+
     End Sub
 
     Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
-        Dim ck3 As Boolean
-        ck3 = CheckBox3.Checked
-        If ck3 = True Then
-            CheckBox1.Enabled = False
-            CheckBox2.Enabled = False
-            CheckBox4.Enabled = False
-        Else
-            CheckBox1.Enabled = True
-            CheckBox2.Enabled = True
-            CheckBox3.Enabled = True
-            CheckBox4.Enabled = True
-        End If
+
     End Sub
 
     Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
-        Dim ck4 As Boolean
-        ck4 = CheckBox4.Checked
-        If ck4 = True Then
-            CheckBox1.Enabled = False
-            CheckBox2.Enabled = False
-            CheckBox3.Enabled = False
-            CheckBox5.Enabled = False
-            CheckBox6.Enabled = False
-            CheckBox7.Enabled = False
-            CheckBox8.Enabled = False
-            CheckBox5.Checked = False
-            CheckBox6.Checked = False
-            CheckBox7.Checked = False
-            CheckBox8.Checked = True
-        Else
-            CheckBox1.Enabled = True
-            CheckBox2.Enabled = True
-            CheckBox3.Enabled = True
-            CheckBox4.Enabled = True
-            CheckBox5.Enabled = True
-            CheckBox6.Enabled = True
-            CheckBox7.Enabled = True
-            CheckBox8.Enabled = True
-            CheckBox5.Checked = False
-            CheckBox6.Checked = False
-            CheckBox7.Checked = False
-            CheckBox8.Checked = False
-        End If
+
     End Sub
 
     Private Sub CheckBox8_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox8.CheckedChanged
-        Dim ck8 As Boolean
-        ck8 = CheckBox8.Checked
-        If ck8 = True Then
-            CheckBox5.Enabled = False
-            CheckBox6.Enabled = False
-            CheckBox7.Enabled = False
-        Else
-            CheckBox5.Enabled = True
-            CheckBox6.Enabled = True
-            CheckBox7.Enabled = True
-            CheckBox8.Enabled = True
-        End If
+
     End Sub
 
     Private Sub CheckBox7_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox7.CheckedChanged
-        Dim ck7 As Boolean
 
-        ck7 = CheckBox7.Checked
-
-        If ck7 = True Then
-            CheckBox5.Enabled = False
-            CheckBox6.Enabled = False
-            CheckBox8.Enabled = False
-        Else
-            CheckBox5.Enabled = True
-            CheckBox6.Enabled = True
-            CheckBox7.Enabled = True
-            CheckBox8.Enabled = True
-        End If
     End Sub
 
     Private Sub CheckBox6_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox6.CheckedChanged
 
-        Dim ck6 As Boolean
-
-        ck6 = CheckBox6.Checked
-        If ck6 = True Then
-            CheckBox5.Enabled = False
-            CheckBox7.Enabled = False
-            CheckBox8.Enabled = False
-        Else
-            CheckBox5.Enabled = True
-            CheckBox6.Enabled = True
-            CheckBox7.Enabled = True
-            CheckBox8.Enabled = True
-        End If
     End Sub
 
     Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox5.CheckedChanged
-        Dim ck5 As Boolean
-        ck5 = CheckBox5.Checked
-        If ck5 = True Then
-            CheckBox6.Enabled = False
-            CheckBox7.Enabled = False
-            CheckBox8.Enabled = False
-        Else
-            CheckBox5.Enabled = True
-            CheckBox6.Enabled = True
-            CheckBox7.Enabled = True
-            CheckBox8.Enabled = True
-        End If
+
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -163,6 +56,14 @@
     End Sub
 
     Private Sub NewChoiceForm01_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ck1 As Boolean
+        Dim cke2 As Boolean
+        ck1 = CheckBox1.Checked
+        If ck1 = True Then
+            TextBox1.Text = "ddddddd"
+        End If
+
+
 
     End Sub
 End Class
