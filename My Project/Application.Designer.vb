727545dbe26fba058fe1ae4b583dbd7653a5987f
恩 姚 <larval@163.com>
2019-09-20 11:34:27 +0800

    ggg

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.MyTest_001.TFrom1
+            Me.MainForm = Global.MyTest_001.NewChoiceForm01
         End Sub
     End Class
 End Namespace
